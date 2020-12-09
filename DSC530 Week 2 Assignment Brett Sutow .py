943394342b250b13182cf149783d3246110d876c
bsutow <70550028+bsutow@users.noreply.github.com>
2020-12-09 08:23:59 -0500

    Add files via upload

---
+++ DSC530 Week 2 Assignment Brett Sutow .py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('Hello World! I wonder why that is always the default coding text to start with')
+
+
+# In[3]:
+
+
+2+2
+
+
+# In[4]:
+
+
+4-2
+
+
+# In[5]:
+
+
+10/5
+
+
+# In[7]:
+
+
+a = "The dog was"
+b= " blue"
+c= a+b
+print(c)
+
+
+# In[10]:
+
+
+Baseball_Players = [ 'Thome', 'Utley', 'Rollins']
+print (Baseball_Players)
+
+
+# In[11]:
+
+
+Baseball_Players.append('Burrell')
+
+
+# In[12]:
+
+
+Baseball_Players
+
+
+# In[13]:
+
+
+Football_Players_Tuple = ('McNabb', 'Owens', 'Westbrook')
+Football_Players_Tuple
+
+
+# In[ ]:
+
+
+
+
